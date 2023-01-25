e7a456bb868504304f5349e269127b398341464c
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2023-01-25 14:08:07 +0200

    ADDED NAVBAR GUI

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import './index.css';
+
 
 const rootelm = document.getElementById('root');
 const root = ReactDOM.createRoot(rootelm);
