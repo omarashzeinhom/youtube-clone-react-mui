f69828cb61321940078c6b7ec2c9cd5c12de50b2
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2023-01-25 19:32:04 +0200

    ADDED VideoCard details

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -21,11 +21,25 @@ const VideoCard = ({
   return (
     <Card>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-        <CardMedia image={snippet?.thumbnails?.high?.url} 
-        alt={snippet?.title}
-        sx={{width: 350, height:150}}
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 350, height: 150,  }}
         />
       </Link>
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "101px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#fff">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+          <Typography variant="subtitle2" fontWeight="bold" color="#fff">
+            {snippet?.channelTitle || demoChannelTitle}
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 };
