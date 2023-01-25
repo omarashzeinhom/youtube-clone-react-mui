4f89c51d1c2621c893e0c4c3cd3287538141eaa5
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2023-01-25 19:23:02 +0200

    ADDED SAMPLE Videos from API

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
 
-const VideoCard = () => {
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  //console.log(videoId, snippet);
+
   return (
-    <div>VideoCard</div>
-  )
-}
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia image={snippet?.thumbnails?.high?.url} 
+        alt={snippet?.title}
+        sx={{width: 350, height:150}}
+        />
+      </Link>
+    </Card>
+  );
+};
 
-export default VideoCard
+export default VideoCard;
